b514caf0c05f5c7215cc437d44b19de89acb3a9d
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2025-12-02 22:14:42 +0530

    add msg

--- controllers/showController.js
+++ controllers/showController.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import Movie from "../models/Movie.js";
 import Show from "../models/Show.js";
 import { set } from "mongoose";
+import { inngest } from "../inngest/index.js";
 
 
 // api to get now playing movies from TMDB api
@@ -71,6 +72,12 @@ export const addShow = async(req,res)=>{
     if(showsToCreate.length > 0){
       await Show.insertMany(showsToCreate);
     }
+
+    // trigger inngest event
+    await inngest.send({
+      name:"app/show.added",
+      data:{movieTitle:movie.title}
+    })
     res.json({success: true, message:"Show Added Successfully"})
 
   } catch (error) {
