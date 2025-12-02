5876c3b6d68cbd2c398c6bd4e04c91435d36b46b
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2025-12-02 20:08:20 +0530

    add mail sender

--- controllers/stripeWebhooks.js
+++ controllers/stripeWebhooks.js
@@ -1,6 +1,7 @@
 import stripe from "stripe"
 // import Booking from "../models/Booking.js"
 import Booking from "../models/Booking.js"
+import { inngest } from "../inngest/index.js";
 
 export const stripeWebhooks  = async (req, res)=>{
   const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
@@ -31,6 +32,13 @@ export const stripeWebhooks  = async (req, res)=>{
         await Booking.findByIdAndUpdate(bookingId, {
           isPaid: true,
           paymentLink: ""
+        })
+
+        // send confirmation email
+
+        await inngest.send({
+          name: "app/show.booked",
+          data: {bookingId}
         })
         break;
       }
