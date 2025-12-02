f4eb2ea80b2d9c5bdbd147d2126761ab9c3ded2b
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2025-12-03 00:11:00 +0530

    asd

--- inngest/index.js
+++ inngest/index.js
@@ -2,7 +2,6 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Booking from "../models/Booking.js"
 import Show from "../models/Show.js"
-import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -86,136 +85,136 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
 
 // inngest function to send email when user books a show 
 
-const sendBookingConfirmationEmail = inngest.createFunction(
-  {id:"send-booking-confirmation-email"},
-  {event: "app/show.booked"},
-  async({event, step})=>{
-    const {bookingId} = event.data;
+// const sendBookingConfirmationEmail = inngest.createFunction(
+//   {id:"send-booking-confirmation-email"},
+//   {event: "app/show.booked"},
+//   async({event, step})=>{
+//     const {bookingId} = event.data;
 
-    const booking = await Booking.findById(bookingId).populate({
-      path: "show",
-      populate: {path: "movie", model: "Movie"}
-    }).populate("user");
-    await sendEmail({
-      to:booking.user.email,
-      subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
-      body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
-      <h2>Hi ${booking.user.name}</h2>
+//     const booking = await Booking.findById(bookingId).populate({
+//       path: "show",
+//       populate: {path: "movie", model: "Movie"}
+//     }).populate("user");
+//     await sendEmail({
+//       to:booking.user.email,
+//       subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+//       body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+//       <h2>Hi ${booking.user.name}</h2>
 
-      <p>
-        Your booking for 
-        <strong style="color: #F84565;">${booking.show.movie.title}</strong> 
-        is confirmed.
-      </p>
+//       <p>
+//         Your booking for 
+//         <strong style="color: #F84565;">${booking.show.movie.title}</strong> 
+//         is confirmed.
+//       </p>
 
-      <p>
-        <strong>Date:</strong> 
-        ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}
-        <br/>
+//       <p>
+//         <strong>Date:</strong> 
+//         ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}
+//         <br/>
         
-        <strong>Time:</strong> 
-        ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
-      </p>
+//         <strong>Time:</strong> 
+//         ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+//       </p>
 
-      <p>Enjoy the show! 🍿</p>
+//       <p>Enjoy the show! 🍿</p>
 
-      <p>
-        Thanks for booking with us!
-        <br/>
-        – QuickShow Team
-      </p>
-     </div>`
-    })
-  }
-)
+//       <p>
+//         Thanks for booking with us!
+//         <br/>
+//         – QuickShow Team
+//       </p>
+//      </div>`
+//     })
+//   }
+// )
 
 
 // inngest function to send reminder
-const sendShowReminders = inngest.createFunction(
-  {id:"send-show-reminders"},
-  {cron: "0 */8 * * *"},//every 8 hours
-  async({step})=>{
-    const now = new Date();
-    const in8Hours = new Date(now.getTime() + 8 * 60 * 60 * 1000)
-    const windowStart = new Date(in8Hours.getTime()- 10 * 60 * 1000)
+// const sendShowReminders = inngest.createFunction(
+//   {id:"send-show-reminders"},
+//   {cron: "0 */8 * * *"},//every 8 hours
+//   async({step})=>{
+//     const now = new Date();
+//     const in8Hours = new Date(now.getTime() + 8 * 60 * 60 * 1000)
+//     const windowStart = new Date(in8Hours.getTime()- 10 * 60 * 1000)
 
-    // prepare reminder task
+//     // prepare reminder task
 
-    const reminderTask = await step.run("prepare-reminder-task", async()=>{
-      const shows = await Show.find({
-        showTime: {$gte : windowStart, $lte: in8Hours},
-      }).populate("movie");
-      const tasks = []
+//     const reminderTask = await step.run("prepare-reminder-task", async()=>{
+//       const shows = await Show.find({
+//         showTime: {$gte : windowStart, $lte: in8Hours},
+//       }).populate("movie");
+//       const tasks = []
 
-      for(const show of shows){
-        if(!shows.movie || !show.occupiedSeats) continue;
-        const userIds = [...new Set(Object.values(show.occupiedSeats))]
-        if(userIds.length === 0 ) continue;
+//       for(const show of shows){
+//         if(!shows.movie || !show.occupiedSeats) continue;
+//         const userIds = [...new Set(Object.values(show.occupiedSeats))]
+//         if(userIds.length === 0 ) continue;
 
-        const users = await User.find({_id: {$in: userIds}}).select("name email");
+//         const users = await User.find({_id: {$in: userIds}}).select("name email");
 
-        for(const user of users){
-          tasks.push({
-            userEmail:user.email,
-            userName: user.name,
-            movieTitle: show.movie.title,
-            showTime: show.showTime,
-          })
-        }
-      }
-      return tasks
-    })
-    if(reminderTask.length === 0 ){
-      return {sent:0, message: "No reminders to send."}
-    }
+//         for(const user of users){
+//           tasks.push({
+//             userEmail:user.email,
+//             userName: user.name,
+//             movieTitle: show.movie.title,
+//             showTime: show.showTime,
+//           })
+//         }
+//       }
+//       return tasks
+//     })
+//     if(reminderTask.length === 0 ){
+//       return {sent:0, message: "No reminders to send."}
+//     }
 
-    // send reminder email
+//     // send reminder email
 
-    const results = await step.run("send-all-reminders", async()=>{
-      return await Promise.allSettled(
-        reminderTask.map(task => sendEmail({
-          to:task.userEmail,
-          subject:`Reminder: Your movie" ${task.movieTitle}" start soon!`,
-          body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
-      <h2>Hi ${task.username}</h2>
+//     const results = await step.run("send-all-reminders", async()=>{
+//       return await Promise.allSettled(
+//         reminderTask.map(task => sendEmail({
+//           to:task.userEmail,
+//           subject:`Reminder: Your movie" ${task.movieTitle}" start soon!`,
+//           body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+//       <h2>Hi ${task.username}</h2>
 
-      <p>
-        Your booking for 
-        <strong style="color: #F84565;">${task.showTime}</strong> 
-        is confirmed.
-      </p>
+//       <p>
+//         Your booking for 
+//         <strong style="color: #F84565;">${task.showTime}</strong> 
+//         is confirmed.
+//       </p>
 
-      <p>
-        <strong>Date:</strong> 
-        ${new Date(task.showTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}
-        <br/>
+//       <p>
+//         <strong>Date:</strong> 
+//         ${new Date(task.showTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}
+//         <br/>
         
-        <strong>Time:</strong> 
-        ${new Date(task.showTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
-      </p>
+//         <strong>Time:</strong> 
+//         ${new Date(task.showTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+//       </p>
 
-      <p>Enjoy the show! 🍿</p>
+//       <p>Enjoy the show! 🍿</p>
 
-      <p>
-        Thanks for booking with us!
-        <br/>
-        – QuickShow Team
-      </p>
-     </div>`
-        }))
-      )
-    })
+//       <p>
+//         Thanks for booking with us!
+//         <br/>
+//         – QuickShow Team
+//       </p>
+//      </div>`
+//         }))
+//       )
+//     })
 
-    const sent = results.filter(r => r.status === "fulfilled").length;
-    const failed = results.length - sent;
+//     const sent = results.filter(r => r.status === "fulfilled").length;
+//     const failed = results.length - sent;
 
-    return {
-      sent,
-      failed,
-      message:`sent ${sent} reminder(s), ${failed} failed`
-    }
-  }
-)
+//     return {
+//       sent,
+//       failed,
+//       message:`sent ${sent} reminder(s), ${failed} failed`
+//     }
+//   }
+// )
 
 
 
